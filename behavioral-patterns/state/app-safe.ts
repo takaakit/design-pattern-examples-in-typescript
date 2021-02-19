44790bb42a82d5f01db0ff865828242db0fdd8fc
takaakit <teshima.takaaki.gh@gmail.com>
2021-02-19 10:40:04 +0900

    Added auto-scrolling process in TextArea

--- behavioral-patterns/state/app-safe.ts
+++ behavioral-patterns/state/app-safe.ts
@@ -83,6 +83,7 @@ export class AppSafe implements Context {
     callSecurityGuardsRoom(message: string): void {
         // ˅
         this.textMessage.value += 'call! ' + message + '\n';
+        this.textMessage.scrollTop = this.textMessage.scrollHeight;     // Scroll to the bottom
         // ˄
     }
 
@@ -90,6 +91,7 @@ export class AppSafe implements Context {
     recordSecurityLog(message: string): void {
         // ˅
         this.textMessage.value += 'record ... ' + message + '\n';
+        this.textMessage.scrollTop = this.textMessage.scrollHeight;     // Scroll to the bottom
         // ˄
     }
 

80aa9f1debb835472902a5c46b92a5b264e0712e
takaakit <takaaki.teshima.dev@gmail.com>
2022-08-14 20:58:38 +0900

    refactoring

--- structural-patterns/facade/data-library.ts
+++ structural-patterns/facade/data-library.ts
@@ -29,7 +29,7 @@ export class DataLibrary {
         // ˅
         const data: Map<string, string> = new Map<string, string>();
 
-        for (let line of fs.readFileSync(dataLibraryFileName, 'utf8').toString().split(/\r\n|\r|\n/)) {
+        for (let line of fs.readFileSync(dataLibraryFileName, `utf8`).toString().split(/\r\n|\r|\n/)) {
             const keyAndValue: string[] = line.split(`=`);
             if (keyAndValue.length === 2) {
                 data.set(keyAndValue[0], keyAndValue[1]);
